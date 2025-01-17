a09809a3e841c9179ff6a7c02fe20177415d3a21
NewExperience <161090277+NewCodingExperience@users.noreply.github.com>
2025-01-17 18:08:15 -0500

    First practice for Python

---
+++ main.py
@@ -0,0 +1,81 @@
+print('Hello world, from VS code')
+# My first line of code.
+print("Hello world!")
+
+# Define the release and runtime integer variables below:
+release_year=2025
+runtime=200
+rating_out_of_10=7.4
+# No semicolon pls.
+
+# Prints "500"
+print(573 - 74 + 1)
+
+# Prints "50"
+print(25 * 2)
+
+
+# Python wants to convert int to double and will respond to double
+# Prints "2.0"
+print(10 / 5)
+
+
+
+# Define the rating_out_of_10 float variable below: 
+
+# Define how to change Python varibles:
+coffee_price = 1.50
+number_of_coffees = 4
+
+# Prints "6.0"
+print(coffee_price * number_of_coffees)
+# Prints "1.5"
+print(coffee_price)
+# Prints "4"
+print(number_of_coffees)
+
+# Updating the price 
+coffee_price = 2.00
+
+# Prints "8.0"
+print(coffee_price * number_of_coffees)
+# Prints "2.0"
+print(coffee_price)
+# Prints "4"
+print(number_of_coffees)
+
+
+# Power rule for Python:
+# 2 to the 10th power, or 1024
+print(2 ** 10)
+
+# 8 squared, or 64
+print(8 ** 2)
+
+# 9 * 9 * 9, 9 cubed, or 729
+print(9 ** 3)
+
+# We can even perform fractional exponents
+# 4 to the half power, or 2
+print(4 ** 0.5)
+
+
+# Modulus operator takes the reminader as the answer.
+# Prints 4 because 29 / 5 is 5 with a remainder of 4
+print(29 % 5)
+ 
+# Prints 2 because 32 / 3 is 10 with a remainder of 2
+print(32 % 3)
+ 
+# Modulo by 2 returns 0 for even numbers and 1 for odd numbers
+# Prints 0
+print(44 % 2)
+
+# plus equals (+=)
+# adds on to the strings and numbers
+
+hike_caption = "What an amazing time to walk through nature!"
+
+# Almost forgot the hashtags!
+hike_caption += " #nofilter"
+hike_caption += " #blessed"
